2b2dccd69d7b6db2a7bf8d8968b7a2dc5250d94f
Hakan <hknhzl@gmail.com>
2024-02-07 22:53:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Evo-14 .repo/local_manifests ;\
 
 # Removals
-rm -rf prebuilts/clang/host/linux-x86 prebuilt/*/webview.apk
+rm -rf external/chromium-webview
 
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 

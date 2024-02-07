dfac0c25c16b2d0622022e2a05056bad9a124edd
Hakan <hknhzl@gmail.com>
2024-02-07 16:28:41 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,12 +14,8 @@ repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 # Clone local_manifests repository
 git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Evo-14 .repo/local_manifests ;\
 
-# Removals
-#rm -rf device/xiaomi/msm8953-common development && \
-
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
-
 
 # Set up build environment
 source build/envsetup.sh && \

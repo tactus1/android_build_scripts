3ee72736a368b04348462e2796f6956e1bbdd955
Hakan <hknhzl@gmail.com>
2024-02-07 16:48:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,14 +8,20 @@ set -e
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
+rm -rf development && \
 # Initialize repo with specified manifest
 repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 
 # Clone local_manifests repository
 git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Evo-14 .repo/local_manifests ;\
+git clone https://github.com/LineageOS/android_development.git -b lineage-21.0 development ;\
+
+# Removals
+#rm -rf device/xiaomi/msm8953-common development && \
 
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+
 
 # Set up build environment
 source build/envsetup.sh && \

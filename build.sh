7eba46c555282ba8fd941333f8e47f065f2b6318
Hakan <hknhzl@gmail.com>
2024-02-09 12:03:50 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
 repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 
 # Clone local_manifests repository
-git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Evo-14 .repo/local_manifests ;\
+git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Evo-test .repo/local_manifests ;\
 
 # Removals
-rm -rf device/xiaomi/msm8953-common device/xiaomi/tissot kernel/xiaomi/msm8953-common prebuilts/clang/host/linux-x86 external/chromium-webview && \
+#rm -rf prebuilts/clang/host/linux-x86 external/chromium-webview && \
 
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 

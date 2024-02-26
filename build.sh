45e11c08a7732a5b1f0b0fd63d98e4f8a3ac0131
Hakan <hknhzl@gmail.com>
2024-02-26 11:11:19 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lf
 git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Alpha-14 .repo/local_manifests ;\
 
 # Removals
-#rm -rf prebuilts/clang/host/linux-x86 external/chromium-webview && \
+# rm -rf prebuilts/clang/host/linux-x86 external/chromium-webview && \
 
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 

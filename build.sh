a06521fcb43670d991d0e826ff15679a080913e2
Hakan <hknhzl@gmail.com>
2024-02-29 12:11:53 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Alpha-14 .r
 rm -rf prebuilts/clang/host/linux-x86 external/chromium-webview && \
 
 # Sync the repositories
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+repo sync -c -j\$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync && \ 
 
 
 # Set up build environment

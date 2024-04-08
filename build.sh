c254b037d30b934c7b439ec0f155df05d64fa892
Tactus <142695427+tactus1@users.noreply.github.com>
2024-04-08 18:20:04 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs 
 # Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
-repo init -u https://github.com/HorizonDroidLab/manifest -b 14 --depth=1 ;\
+repo init -u https://github.com/HorizonDroidLab/manifest -b 14-qpr2 --depth=1 ;\
 
 # Clone local_manifests repository
 git clone https://github.com/tactus1/local_manifest --depth 1 -b horizondroid-14-tissot .repo/local_manifests ;\
@@ -25,7 +25,7 @@ git clone https://github.com/tactus1/local_manifest --depth 1 -b horizondroid-14
 . build/envsetup.sh && \
 
 # Lunch configuration
-lunch aosp_tissot-ap1a-userdebug ;\
+lunch horizon_tissot-ap1a-userdebug ;\
 
 croot ;\
 m bacon ; \

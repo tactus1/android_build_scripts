79f4863700e3eba61af7617d29545647f95a81a3
Tactus <142695427+tactus1@users.noreply.github.com>
2024-04-11 23:22:38 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,13 +9,14 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs 
 # Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
-repo init -u https://github.com/yaap/manifest.git -b fourteen --depth=1 ;\
+repo init -u https://github.com/yaap/manifest.git -b fourteen --git-lfs --depth=1 ;\
 
 # Clone local_manifests repository
 git clone https://github.com/tactus1/local_manifest --depth 1 -b YAAP-14-tissot .repo/local_manifests ;\
 
 # Removals
 # rm -rf device/xiaomi/msm8953-common prebuilts/clang/host/linux-x86 external/chromium-webview && \
+rm -rf prebuilts 
 
 # Sync the repositories
 /opt/crave/resync.sh && \
@@ -25,7 +26,7 @@ git clone https://github.com/tactus1/local_manifest --depth 1 -b YAAP-14-tissot 
 source build/envsetup.sh && \
 
 # Lunch configuration
-lunch yaap_tissot-ap1a-userdebug ;\
+lunch yaap_tissot-userdebug ;\
 
 croot ;\
 m yaap ; \

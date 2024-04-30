5c32d834044b3d8618ae63709a8401d16848ef3f
Tactus <142695427+tactus1@users.noreply.github.com>
2024-04-30 16:07:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs 
 # Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
-repo init -u https://github.com/ProjectMatrixx/android -b 14.0 --depth=1 ;\
+repo init -u https://github.com/CherishOS/android_manifest -b 14.0 --depth=1 ;\
 
 # Clone local_manifests repository
-git clone https://github.com/tactus1/local_manifest --depth 1 -b matrixx-14-tissot .repo/local_manifests ;\
+git clone https://github.com/tactus1/local_manifest --depth 1 -b cherish-14-tissot .repo/local_manifests ;\
 
 # Removals
 # rm -rf device/xiaomi/msm8953-common prebuilts/clang/host/linux-x86 external/chromium-webview && \

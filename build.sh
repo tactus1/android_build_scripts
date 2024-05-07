68883dacb02a0b0894d1bbd36ac979532ea5563b
Tactus <142695427+tactus1@users.noreply.github.com>
2024-05-07 16:21:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
+repo init -u https://github.com/CipherOS/android.git -b fourteen-qpr --git-lfs --depth=1
 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
+crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
 repo init -u https://github.com/CherishOS/android_manifest -b uqpr2 ;\
 

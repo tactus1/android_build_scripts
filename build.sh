729c7164754a9c455e69a9a8429abc5d9471cfaa
Tactus <142695427+tactus1@users.noreply.github.com>
2024-08-05 23:07:03 +0300

    Dirty build

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs 
 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
-crave run  --clean --no-patch -- "rm -rf .repo/local_manifests ;\
+crave run --no-patch -- "rm -rf .repo/local_manifests ;\
 # Initialize repo with specified manifest
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --depth=1 ;\
 
@@ -31,6 +31,7 @@ croot ;\
 # Lunch configuration
 # lunch lineage_raphael-ap2a-user ;\
 brunch raphael ;\
+make bacon ;\
 # echo "Date and time:" ; \
 
 # Print out/build_date.txt
